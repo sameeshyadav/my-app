455a889902cace9a1e2fb0496c32c41429c7da0d
sameesh yadav <sameeshyadav@gmail.com>
2021-12-16 11:31:27 +0530

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          HOLA ANK
-        </h1>
-      </header>
-    </div>
+      <div style={{border:'2rem'}}>
+        <Card style={{ width: '18rem',border:'2px'}}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
   );
 }
 
