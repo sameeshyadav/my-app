f784daac781469899c69c2de23156d95210c6384
sameesh yadav <sameeshyadav@gmail.com>
2021-12-16 16:32:28 +0530

    blank

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 function App() {
   return (
-      <div style={{border:'2rem'}}>
+      <div class="d-flex justify-content-center">
         <Card style={{ width: '18rem',border:'2px'}}>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="primary">Blinks GM</Button>
           </Card.Body>
         </Card>
       </div>
