2be393567de75ab31473803cdc50157cc786b134
sameesh <sameeshyadav@gmail.com>
2024-10-31 21:06:55 +0530

    Adding profile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import Profile from './Componets/Profile';
 function App() {
   return (
       <div class="d-flex justify-content-center">
+        <Profile/>
         <Card style={{ width: '18rem',border:'2px'}}>
           <Card.Body>
             <Button variant="primary">Hi Ank</Button>
